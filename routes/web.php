ff21621b3ea3c143d2b94845d9adda4da39f2718
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-21 11:44:05 +0700

    memperbaiki routing

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pegawai', 'PegawaiController@index');
+Route::get('/pegawai', 'App\Http\Controllers\PegawaiController@index');
