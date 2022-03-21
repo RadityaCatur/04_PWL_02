c878a74fc3bc26eb6611b62d79aab6c89ad0e3a9
Raditya Catur Narendra <2041720198@student.polinema.ac.id>
2022-03-21 11:35:08 +0700

    membuat controller

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Pegawai;
+
 class PegawaiController extends Controller
 {
-    //
+    public function index()
+    {
+        //mengambil data pegawai
+        $pegawai = Pegawai::all();
+
+        //mengirim data pegawai ke view pegawai
+        return view('pegawai', ['pegawai' => $pegawai]);
+    }
 }
